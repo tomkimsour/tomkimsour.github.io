e3e2d97e9ae39d254177db3cbdc8112a3402f217
Thomas Ung <tomkimsour@gmail.com>
2023-11-24 23:54:26 +0100

    fix typo

--- portfolio-1069d735515e2dbd.js
+++ portfolio-1069d735515e2dbd.js
@@ -977,7 +977,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('portfolio-241a4e17d75a379_bg.wasm', import.meta.url);
+        input = new URL('portfolio-1069d735515e2dbd_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
